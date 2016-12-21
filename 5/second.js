af2c63ed17bf2b7663bd40bed050f8ee92d3570c
Nikola <nikkop@users.noreply.github.com>
2016-12-21 10:56:26 +0100

    Update second.js

--- 5/second.js
+++ 5/second.js
@@ -9,19 +9,3 @@ for (var i = 0; Object.keys(pwd).length < 8; i++) {
 }
 
 console.log(Object.keys(pwd).map(key => pwd[key]).join(''));
-
-// 'use strict';
-// const md5 = require('md5');
-// const id = 'abc';
-// let pwd = [];
-//
-// console.log(pwd)
-//
-// for (var i = 0; pwd.length < 8; i++) {
-//   let hash = md5(`${id}${i}`);
-//   if(hash.startsWith('00000')) console.log('Inserting #' + hash.charAt(6) + ' at index: ' + hash.charAt(5))
-//   if(hash.startsWith('00000') && hash.charAt(5) < 8 && pwd[hash.charAt(5)] == undefined) pwd.splice(hash.charAt(5), 0, hash.charAt(6));
-//   if(hash.startsWith('00000') && hash.charAt(5) < 8 && pwd[hash.charAt(5)] == undefined) console.log(pwd);
-// }
-//
-// console.log(pwd.join(''))
